faa0cdf94c0adbfe60cd7f9c74f938e7fa9e9e44
pss-aileen <140990100+pss-aileen@users.noreply.github.com>
2023-12-02 14:20:00 +0800

    💕 FEATURE: 簡単な関数を作成 @ main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,61 @@
 'use strict';
 {
 
+  const generateBtn = document.getElementById("btn-generate");
 
+  generateBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    getInput();
+  });
 
+  function getInput() {
+    const commitForm = document.forms["commitForm"];
+  
+    const subject = commitForm["subject"].value;
+    const prefixOption = commitForm["prefixOption"].value;
+    const commentSwitch = commitForm["commentSwitch"].value;
+    const comment = commitForm["comment"].value;
+    const issue = commitForm["issue"].value;
+  
+    // console.log(prefixOption);
+    // console.log(subject);
+    // console.log(commentSwitch);
+    // console.log(comment);
+    // console.log(issue);
+
+    return { subject, prefixOption, commentSwitch, comment, issue };
+  }
+
+  messageOutput();
+
+  function messageOutput() {
+    const { subject, prefixOption, commentSwitch, comment, issue } = getInput();
+    console.log(subject);
+    console.log(prefixOption);
+    console.log(commentSwitch);
+    console.log(comment);
+    console.log(issue);
+
+    const messageOutputTarget = document.getElementById("message-output");
+    console.log(messageOutputTarget.value);
+
+    if (commentSwitch === "true") {
+      console.log("on");
+
+      messageOutputTarget.value = `git commit -m "${prefixOption} FEATURE: ${subject} (${issue})" -m "${comment}"`;
+
+    } else {
+      console.log("off");
+
+      messageOutputTarget.value = `git commit -m "${prefixOption} FEATURE: ${subject} (${issue})" -m "${comment}"`;
+
+    }
+  }
+
+  
+  
+  
+
+  // const generatedMessage = commitForm["generatedMessage"];
+  // console.log(generatedMessage.value);
 }
