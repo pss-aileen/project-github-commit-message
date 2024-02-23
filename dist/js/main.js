fcdf406cabca58ebf9dca49c9d654d67e66004ba
pss-aileen <140990100+pss-aileen@users.noreply.github.com>
2024-02-23 17:57:39 +0800

    💕 FEAT: データ取得実装途中
    class JsonDataを作成

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,68 @@
 'use strict';
 {
+
+  class JsonData {
+    constructor(url) {
+      this.url = url;
+      this.data = null;
+    }
+
+    async loadData() {
+      try {
+        const response = await fetch(this.url);
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        this.data = await response.json();
+        console.log("Data loaded successfully");
+      } catch (error) {
+        console.error('Error loading JSON data:', error);
+      }
+    }
+
+    
+  }
   
+  const fetchData = async () => {
+    const res = await fetch("./data/data.json")
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("NG");
+        }
+        return response.json();
+      })
+      .catch(error => {
+        console.error("no data");
+        return "no data";
+      })
+  };
+
+  const fetchData2 = async () => {
+    return new Promise((resolve, reject) => {
+      fetch("./data/data.json")
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("NG");
+        }
+        resolve(response.json());
+      })
+      .catch(error => {
+        console.error("no data");
+        reject("no data");
+      })
+    });
+  }
+
+
+  const getPrifixType = async() => {
+    const prefixList = await fetchData2();
+    const eachPrefix = prefixList.map((item) => {
+      return item.displayName;
+    });
+    console.log(eachPrefix);
+  };
+  getPrifixType();
+
+
 
 } // end
