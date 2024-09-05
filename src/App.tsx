cd611972ddb1b8117b6a0a4bfa6397030d60371a
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-09-05 12:19:22 +0900

    install tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return <>a</>;
+  return <h1 className='text-3xl font-bold underline bg-purple-600'>Hello world!</h1>;
 }
 
 export default App;
